f1d1debe51df89eac871183795a043a1f6c92706
Werazo22 <guero-37@hotmail.com>
2023-10-17 14:19:13 -0600

    18 Captura

--- src/Componentes/Tortas.jsx
+++ src/Componentes/Tortas.jsx
@@ -17,28 +17,30 @@ const Tortas = () => {
                                 <div className="food">
                                     <h3 className="n_food"><span className="number">1</span> Asi son las Tortas</h3>
                                     <p>
-                                        El taco es una preparación culinaria muy popular de México 
-                                        que consiste en una tortilla, generalmente de maíz, 
-                                        que comúnmente se enrolla para contener dentro diversos 
-                                        ingredientes y algún tipo de salsa y verdura.
+                                        Las Tortas son un tipo de sándwich preparado con un bollo de pan llamado "telera" o "bolillo". 
+                                        Estos panes son de forma ovalada, y se cortan por la mitad para ser rellenados con diversos ingredientes.
+                                        <br/>
+                                        Las tortas pueden ser muy variadas y se pueden rellenar con diferentes tipos de carnes, embutidos, quesos y complementos.
                                     </p>
                                 </div>
                                 <div className="food">
                                     <h3 className="n_food"><span className="number">2</span> Historia</h3>
                                     <p>
-                                        En diferentes culturas se desarrolló la tradición de una preparación 
-                                        a base de harina que aporta carbohidratos hecha de tal manera que 
-                                        queda como una superficie plana que sirve de sostén para otros alimentos.
-                                        Así, se puede comparar el taco con el shawarma de origen turco.
+                                        El origen de la torta mexicana es un tema interesante que combina la influencia europea con la adaptabilidad y 
+                                        creatividad de la gastronomía mexicana.
+                                        <br/>
+                                        Aunque las historias exactas pueden variar, lo que es indiscutible es que la torta mexicana es el resultado de la mezcla de tradiciones 
+                                        culinarias y la innovación local. 
+                                        Con el tiempo, ha evolucionado y se ha consolidado como uno de los platillos más representativos y queridos de México.
                                     </p>
                                 </div>
                                 <div className="food">
                                     <h3 className="n_food"><span className="number">3</span> Tipos de Tortas</h3>
                                     <p>
                                         Se pueden distinguir algunos tipos particulares, 
-                                        basados en su tipo de carne o en su preparación. Hay quienes consi deran que un taco 
+                                        basados en su tipo de carne o en su preparación. Hay quienes consideran que una torta 
                                         nace con el «mero acto» de 
-                                        envolver o enrollar un guiso con una tortilla.
+                                        rellenar un bolillo.
                                     </p>
                                 </div>
                             </div>
